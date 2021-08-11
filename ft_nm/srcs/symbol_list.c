f0bf1bd2161df2fe1f05876ab915fe19eb5c05da
mpivet-p <mpivet-p@student.42.fr>
2021-08-11 16:32:15 +0200

    nm sort fixed

--- ft_nm/srcs/symbol_list.c
+++ ft_nm/srcs/symbol_list.c
@@ -3,31 +3,25 @@
 
 static int		is_sorted(t_list *a, t_list *b, void const *str)
 {
-	size_t	i = 0, j = 0, len;
+	size_t	i = 0, j = 0;
 	int		ret = 0;
-	char	s1[128];
-	char	s2[128];
+	char	*s1;
+	char	*s2;
 
-	ft_strncpy(s1, (char*)(str + ((Elf64_Sym*)a->content)->st_name), 128);
-	ft_strncpy(s2, (char*)(str + ((Elf64_Sym*)b->content)->st_name), 128);
-	len = ft_strlen(s1);
-	for (size_t ii = 0; ii < len; ii++)
-		s1[ii] = ft_tolower(s1[ii]);
-	len = ft_strlen(s2);
-	for (size_t ii = 0; ii < len; ii++)
-		s2[ii] = ft_tolower(s2[ii]);
+	s1 = (char*)(str + ((Elf64_Sym*)a->content)->st_name);
+	s2 = (char*)(str + ((Elf64_Sym*)b->content)->st_name);
 	while (s1[i] && s2[j])
 	{
 		while (s1[i] && s1[i] == '_')
 			i++;
 		while (s2[j] && s2[j] == '_')
 			j++;
-		if (!s1[i] || s1[i] != s2[j])
+		if (!s1[i] || ft_tolower(s1[i]) != ft_tolower(s2[j]))
 			break ;
 		i++;
 		j++;
 	}
-	ret = s1[i] - s2[j];
+	ret = ft_tolower(s1[i]) - ft_tolower(s2[j]);
 	if (ret == 0)
 	{
 		return (ELF64_ST_BIND(((Elf64_Sym*)a->content)->st_info)
